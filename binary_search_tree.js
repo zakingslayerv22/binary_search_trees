e716f7d71d6375ce555d470216fce1dddad6c8ca
Eriwe <11926638+zakingslayerv22@users.noreply.github.com>
2025-02-25 09:44:19 -0800

    Initialize a prepArray() method that prepares an array for a tree to be built out of it.
    Take in an array and
    remove duplicates by using
    filter() to go
    through the array and using
    indexOf() to keep only the
    first occurence of each
    element. For each element,
    it checks if the index
    returned by indexOf()
    (which is the first
    index where that element
    appears) is equal to the
    current index. If so, it
    includes the element;
    otherwise, it skips it.
    
    Since filter() returns an
    array, sort the array
    returned from filter()
    
    Return the array.
    
    This removes duplicates
    from andand sorts the
    array that was passed
    into it.

--- binary_search_tree.js
+++ binary_search_tree.js
@@ -2,6 +2,13 @@ export class Tree {
   constructor(array) {
     this.array = array;
   }
+
+  prepArray(array) {
+    //remove duplicates and sort array
+    return array
+      .filter((element, index) => array.indexOf(element) === index)
+      .sort((a, b) => a - b);
+  }
 }
 
 export class Node {
